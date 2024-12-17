c2b7d063d08a751ea6434d78fcc3f7c94f45a5f6
TheounSeyha <149511852+TheounSeyha@users.noreply.github.com>
2024-12-17 23:52:53 +0700

    pratice grid

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  darkMode: "class", // or 'media'
+  content: ["C:UsersUSERDesktopCSTADweek1-HTMLpractice2.html.{html,js}"], // Adjust paths to your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  darkMode: "class", // or 'media'
+  content: ["C:UsersUSERDesktopCSTADweek1-HTML"], // Adjust paths to your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
